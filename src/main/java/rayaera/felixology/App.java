076731375d0bc861d527cf2d418cdbd70ac47416
Raymond Reynoso <r-reynoso@live.com>
2015-03-04 19:21:57 -0400

    Start commit

---
+++ src/main/java/rayaera/felixology/App.java
@@ -0,0 +1,13 @@
+package rayaera.felixology;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
