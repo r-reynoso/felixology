89037a1ae4d9f23bde09daba3ab82e46aeef3077
Raymond Reynoso <r-reynoso@live.com>
2015-08-05 21:49:11 -0400

    First commit

---
+++ src/main/webapp/js/fcalc.js
@@ -0,0 +1,52 @@
+function imc()
+ 		{
+ 			var piesForm = document.getElementById('pies').value; 						// busca valor del textbox
+ 			var pulgadasForm = document.getElementById('pulgadas').value;
+ 			var librasForm = document.getElementById('libras').value;
+ 																						// validar si hay campos y si son numericos
+ 				if (piesForm == null || piesForm == "" || pulgadasForm == null || pulgadasForm == "" || librasForm == null || librasForm == "")
+	 				{
+	 					alertify.alert("Tienes que llenar todo los campos!");
+	 				}
+ 				else if (isNaN(piesForm) || isNaN(pulgadasForm) || isNaN(librasForm))
+ 					{
+						alertify.alert("Todos los datos tiene que ser numericos!")
+ 					}
+	 			else
+	 				{
+			 			var pies = parseInt(piesForm);									// convertir valor in int
+			 			var pulgadas = parseInt(pulgadasForm); 
+			 			var libras = parseInt(librasForm);	
+			 			
+			 			var altura = pies * 12 + pulgadas;								// calcular altura en pulgadas
+			 			var imcForm = libras * 703 / Math.pow(altura, 2);				// calcular imc
+			 			var imc = imcForm.toFixed(1);
+			 			
+			  			alertify.alert("Tu indice de masa corporal es: " + imc);
+			 			
+	 				}
+ 				
+ 		}
+function igchombre()
+		{
+			var librasForm = document.getElementById('libras').value;					// busca valor del textbox
+			var cinturaFrom = document.getElementById('cintura').vaule;
+																						// validar si hay campos y si son numericos
+				if (librasForm == null || librasForm == "" || cinturaForm == null || cinturaForm == "")
+					{
+						alertify.alert("Tienes que llenar todo los campos!");
+					}
+				else if (isNaN(librasForm) || isNaN(cinturaForm))
+					{
+					alertify.alert("Todos los datos tiene que ser numericos!")
+					}
+				else
+					{
+		 			var libras = parseInt(librasForm);										// convertir valor in int
+		 			var cintura = parseInt(cinturaForm); 
+		 			
+		 			var resultado1 = libras * 1.082 + 94.42;								// calcular igc para el hombre
+		 			var resultado2 = cintura * 4.15;
+					}						
+						
+		}
