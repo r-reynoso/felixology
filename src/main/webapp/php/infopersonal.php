056e15198ac46ac71782a4abc7107ed599a0ba35
Raymond Reynoso <r-reynoso@live.com>
2016-03-02 20:34:22 -0400

    Added a infopersonal.php file for data conectivity and made the submit button on the registration html.

---
+++ src/main/webapp/php/infopersonal.php
@@ -0,0 +1,9 @@
+<?php
+// we connect to localhost
+$link = mysql_connect('localhost', 'rayaera', 'Maricela1765');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
+?>
