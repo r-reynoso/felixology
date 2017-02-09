87698ae74d149abfb2a1cdc9fcfd5c69cd332071
Raymond Reynoso <r-reynoso@live.com>
2017-02-09 11:17:11 -0400

    Eliminated the dbconnection.php and added that script to dbiniciarseccion.php, dbrecuperacionpw.php and dbregistracion.php.
    Change error mesage on errorconn.html.

--- src/main/webapp/php/dbiniciarseccion.php
+++ src/main/webapp/php/dbiniciarseccion.php
@@ -1,21 +1,29 @@
 <?php
-		//Include connection.php file.
-		include 'connection.php';
+     	// Database conection configuration
+		$servername = "10.30.84.161";
+		$username = "rayaera";
+		$password = "Maricela1765";
+		$database = "registracion";
+
+		// database connection error handeler
+		$conn = mysqli_connect($servername, $username, $password, $database);
+			if (!$conn)
+				{
+					header('Location: /errorconn.html');
+					exit;
+				}
+			
+		//----------------------------------------------------------------------------
 		
 		//Starting all of the variables connections in and out of this php page.
 		session_start();
-		
-		//Getting connection value from connection.php.
-		$conn= $_SESSION['connection'];
-		
-		//----------------------------------------------------------------------------
-		
+				
 		//Getting data from iniciacion.php
 		$value1 = $_POST['email'];
 		$value2 = md5($_POST['password']);
 				
 			
-		//Querry and email and password validation else error connection.
+		//Querry email, and password validation else error connection.
 		$sql = "SELECT email, password From informacionpersonal WHERE email = '$value1' ";		
 		$result = $conn->query($sql);
 		
--- src/main/webapp/php/dbregistracion.php
+++ src/main/webapp/php/dbregistracion.php
@@ -1,7 +1,17 @@
-<!-- Database connection script -->>
 <?php
-		//Include connection.php file.
-		include 'connection.php';
+		// Database conection configuration
+		$servername = "10.30.84.161";
+		$username = "rayaera";
+		$password = "Maricela1765";
+		$database = "registracion";
+		
+		// database connection error handeler
+		$conn = mysqli_connect($servername, $username, $password, $database);
+			if (!$conn)
+			{
+				header('Location: /errorconn.html');
+				exit;
+			}
 
 		//Starting all of the variables connections in and out of this php page.		
 		session_start();
