0327b81af64aba5a47b682683415b3c251237826
Raymond Reynoso <r-reynoso@live.com>
2017-02-27 09:06:32 -0500

    Templates email para recuperacion de password.

--- src/main/webapp/php/dbrecuperacionpw.php
+++ src/main/webapp/php/dbrecuperacionpw.php
@@ -31,14 +31,60 @@
 		{
 			while($row = $result->fetch_assoc()) {
 				
-				$pw = $row["password"];
+				/*$pw = $row["password"];
 				
 				$to = "$value1";
 				$subject = "Recuperacion Password";
-				$txt = "Su contrasena es $pw."  ;
+				$message = 
+				"
+						
+						
+				";
 				$headers = "From: Rayaera" . "\r\n" .	"CC: somebodyelse@example.com";
 				
-				mail($to,$subject,$txt,$headers);
+				mail($to,$subject,$message,$headers); */
+				
+				$to = "$value1";
+				$subject = "Beautiful HTML Email using PHP by CodexWorld";
+				
+				$htmlContent = '
+								    <html>
+								    <head>
+								        <title>Welcome to CodexWorld</title>
+								    </head>
+								    <body>
+								        <h1>Thanks you for joining with us!</h1>
+								        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 300px; height: 200px;">
+								            <tr>
+								                <th>Name:</th><td>CodexWorld</td>
+								            </tr>
+								            <tr style="background-color: #e0e0e0;">
+								                <th>Email:</th><td>contact@codexworld.com</td>
+								            </tr>
+								            <tr>
+								                <th>Website:</th><td><a href="http://www.codexworld.com">www.codexworld.com</a></td>
+								            </tr>
+								        </table>
+								    </body>
+								    </html>
+								';
+				
+				// Set content-type header for sending HTML email
+				$headers = "MIME-Version: 1.0" . "\r\n";
+				$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+				
+				// Additional headers
+				$headers .= 'From: CodexWorld<sender@example.com>' . "\r\n";
+				$headers .= 'Cc: welcome@example.com' . "\r\n";
+				$headers .= 'Bcc: welcome2@example.com' . "\r\n";
+				
+				// Send email
+				if(mail($to,$subject,$htmlContent,$headers)):
+				$successMsg = 'Email has sent successfully.';
+				else:
+				$errorMsg = 'Email sending fail.';
+				endif;
+				
 			}
 		}
 		 
